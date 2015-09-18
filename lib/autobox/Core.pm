d5041b654c730b0aacb870ef699ba5dfda9aef90
Tomasz Konojacki <me@xenu.tk>
2015-09-18 21:06:20 +0000

    Import of XENU/autobox-Core-1.29.tar.gz from CPAN.
    gitpan-cpan-distribution: autobox-Core
    gitpan-cpan-version:      1.29
    gitpan-cpan-path:         XENU/autobox-Core-1.29.tar.gz
    gitpan-cpan-author:       XENU
    gitpan-cpan-maturity:     released

--- lib/autobox/Core.pm
+++ lib/autobox/Core.pm
@@ -41,7 +41,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.28';
+our $VERSION = '1.29';
 
 use base 'autobox';
 
@@ -1567,6 +1567,8 @@ sub flip {
 #
 ##############################################################################################
 package autobox::Core::ARRAY;
+
+no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
 use constant FIVETEN => ($] >= 5.010);
 
