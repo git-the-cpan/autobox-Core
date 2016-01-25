de6e3e4a1d237f90af5275740e57114d2fc6a0b8
Tomasz Konojacki <me@xenu.tk>
2016-01-25 18:35:47 +0000

    Import of XENU/autobox-Core-1.32.tar.gz from CPAN.
    gitpan-cpan-distribution: autobox-Core
    gitpan-cpan-version:      1.32
    gitpan-cpan-path:         XENU/autobox-Core-1.32.tar.gz
    gitpan-cpan-author:       XENU
    gitpan-cpan-maturity:     released

--- lib/autobox/Core.pm
+++ lib/autobox/Core.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.31';
+our $VERSION = '1.32';
 
 use base 'autobox';
 
@@ -297,7 +297,7 @@ list of values.
 
 String substitution.  Works similarly to C<< s/// >>.
 In boolean context, it returns true/false to indicate whether the substitution succeeded.  C<if>, C<?:>, C<!>, and so on, all provide boolean context.
-It either fails or succeeds, having replaced only one occurance on success -- it doesn't replace globally.
+It either fails or succeeds, having replaced only one occurrence on success -- it doesn't replace globally.
 In scalar context other than boolean context, it returns the modified string (incompatible change, new as of v 1.31).
 
 =head4 undef
